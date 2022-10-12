523f7e25ff6863990b0c3c83dea72fb144dd49ef
Calvin Glisson <cglis001@ucr.edu>
2022-10-11 18:23:50 -0700

    Implement BFS and update node class with path and cost vectors

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <deque>
 
 const bool primitive_test = true; // Whether or not to run the lazy cout tests
 
@@ -22,16 +24,26 @@ private:
     const std::string state; // State that this node corresponds to
     const unsigned heuristic; // Heuristic value of this node
     std::vector<edge> edges; // All edges from this node to another, with path cost
+    std::vector<std::string> path; // List, in order, of states traversed to reach this node
+    std::vector<unsigned> costs; // List in order of accumulated path costs along path
 public:
-    node(std::string s, unsigned h, std::vector<edge> e)
-        : state(s), heuristic(h), edges(e) { };
+    node(std::string s, unsigned h)
+        : state(s), heuristic(h) { };
 
     std::string getState() { return state; };
+    unsigned getHeuristic() { return heuristic; };
     edge getEdge(unsigned i) { return edges.at(i); };
     unsigned getEdgeCount() { return edges.size(); };
-    unsigned getHeuristic() { return heuristic; };
+    std::string getPathMember(unsigned i) { return path.at(i); };
+    unsigned getPathSize() { return path.size(); };
+    unsigned getCostsMember(unsigned i) { return costs.at(i); };
+    unsigned getCostsSize() {return costs.size(); };
 
     void addEdge(edge e) { edges.push_back(e); };
+    void addToPath(std::string s) { path.push_back(s); };
+    void clearPath() { path.clear(); };
+    void addToCosts(unsigned n) { costs.push_back(n); };
+    void clearCosts() { costs.clear(); };
 };
 
 std::string delTrailingWhitespace(std::string &input) {
@@ -49,6 +61,106 @@ int getHeuristicFromVector(std::string state, std::vector<heuristic_data> data) 
     std::cerr << "Failed to find heuristic information for a node of state " << state
               << std::endl;
     return -1;
+}
+
+int getIndexOfState(std::vector<node> graph, std::string state) {
+    for (int i = 0; i < graph.size(); ++i) {
+        if (graph.at(i).getState() == state) {
+            return i;
+        }
+    }
+
+    // Failure
+    std::cerr << "Failed to find a node of state " << state << std::endl;
+    return -1;
+}
+
+node BFS(std::vector<node> graph, unsigned start_index, unsigned goal_index) {
+    // Frontier and explored contain indices of nodes
+    std::deque<unsigned> frontier; // Need deque to be able to search the frontier easily
+    std::vector<unsigned> explored;
+    frontier.push_front(start_index); // Initial state of frontier is just the start node
+
+    // Initialize start state's path and costs vectors
+    graph.at(start_index).addToPath(graph.at(start_index).getState());
+    graph.at(start_index).addToCosts(0);
+
+    while (!frontier.empty()) {
+        unsigned n = frontier.front();
+        frontier.pop_front();
+
+        // Goal test
+        if (n == goal_index) {
+            return graph.at(n);
+        }
+
+        explored.push_back(n);
+
+        // Tie break: edge that appears earliest in input file (lowest index in node.edges)
+        // is enqueued first
+        for (int i = 0; i < graph.at(n).getEdgeCount(); ++i) {
+            std::string child_state = graph.at(n).getEdge(i).end_state;
+            unsigned child_state_index = getIndexOfState(graph, child_state);
+
+            // Ignore children that are already in explored or frontier
+            bool already_in_explored = std::find(explored.begin(), explored.end(),
+                                                 child_state_index) != explored.end();
+            bool already_in_frontier = std::find(frontier.begin(), frontier.end(),
+                                                 child_state_index) != frontier.end();
+            if (!already_in_explored && !already_in_frontier) {
+                // Set child's path vector to the parent's
+                graph.at(child_state_index).clearPath();
+                for (int j = 0; j < graph.at(n).getPathSize(); ++j) {
+                    graph.at(child_state_index).addToPath(graph.at(n).getPathMember(j));
+                }
+                graph.at(child_state_index).clearCosts();
+                for (int j = 0; j < graph.at(n).getCostsSize(); ++j) {
+                    graph.at(child_state_index).addToCosts(graph.at(n).getCostsMember(j));
+                }
+
+                // Now update path/costs to account for the transition from parent to child
+                graph.at(child_state_index).addToPath(child_state);
+                unsigned parentCost =
+                    graph.at(n).getCostsMember(graph.at(n).getCostsSize() - 1);
+                graph.at(child_state_index).addToCosts(parentCost
+                                                       + graph.at(n).getEdge(i).cost);
+
+                // Finally add the child to the frontier
+                frontier.push_back(child_state_index);
+            }
+        }
+    }
+
+    // Only reach this line if fail to find a solution in the loop, so return failure
+    return node("", 0); // Represent failure as a stateless node
+}
+
+node DFS(std::vector<node> graph, unsigned start_index, unsigned goal_index) {
+
+}
+
+node UCS(std::vector<node> graph, unsigned start_index, unsigned goal_index) {
+    return graph.at(0);
+}
+
+node A_STAR(std::vector<node> graph, unsigned start_index, unsigned goal_index) {
+
+}
+
+node getSolution(std::vector<node> graph,
+                 unsigned start_index, unsigned goal_index, std::string alg) {
+    if (alg == "BFS") {
+        return BFS(graph, start_index, goal_index);
+    } else if (alg == "DFS") {
+        return DFS(graph, start_index, goal_index);
+    } else if (alg == "UCS") {
+        return UCS(graph, start_index, goal_index);
+    } else if (alg == "A*") {
+        return A_STAR(graph, start_index, goal_index);
+    } else {
+        std::cerr << "Invalid algorithm name \"" << alg << "\". Defaulting to UCS." << std::endl;
+        return UCS(graph, start_index, goal_index);
+    }
 }
 
 int main() {
@@ -157,10 +269,9 @@ int main() {
         } else { // Otherwise, create new node with this edge
             // Have to set the heuristic value while creating a new node
             const int h = getHeuristicFromVector(node_state, sunday_traffic_data);
-            std::vector<edge> e;
-            e.push_back(live_traffic_data.at(i));
 
-            node n(node_state, h, e);
+            node n(node_state, h);
+            n.addEdge(live_traffic_data.at(i));
             graph.push_back(n);
         }
     }
@@ -179,9 +290,7 @@ int main() {
         // If not, it must be a node with no edges leaving it, since already added the rest
         if (!node_already_exists) {
             const int h = getHeuristicFromVector(node_state, sunday_traffic_data);
-            std::vector<edge> e; // Leave vector empty since this node has no edges
-
-            node n(node_state, h, e);
+            node n(node_state, h);
             graph.push_back(n);
         }
     }
@@ -210,6 +319,29 @@ int main() {
     }
 
     /* Apply the specified algorithm and find out the solution */
+    // Start state is not necessarily the first node in the graph vector
+    unsigned start_index = getIndexOfState(graph, start_state);
+    unsigned goal_index = getIndexOfState(graph, goal_state);
+
+    node solution = getSolution(graph, start_index, goal_index, algo);
+
+    if (solution.getState() == "") { // Empty state represents failure
+        std::cout << "Failed to find a solution.";
+
+        return -1;
+    }
+
+    if (primitive_test) {
+        std::cout << "Found solution node of state " << solution.getState()
+                  << "." << std::endl;
+
+        std::cout << "Solution path is: " << std::endl;
+        for (int i = 0; i < solution.getPathSize(); ++i) {
+            std::cout << solution.getPathMember(i)
+                      << " reached with total cost "
+                      << solution.getCostsMember(i) << std::endl;
+        }
+    }
 
     /* Output the solution to output.txt */
 
